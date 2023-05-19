fe0dfbc111afb55d2881a8d7807b12eda08674df
dale hornidge <dalehornidge@gmail.com>
2023-05-19 15:02:20 +0100

    pilot class created and tested

---
+++ src/main/java/Pilot.java
@@ -0,0 +1,36 @@
+public class Pilot {
+
+    String name;
+    CabinCrewRanks cabinCrewRanks;
+    String pilotLicenceNumber;
+
+    public Pilot(String name, CabinCrewRanks cabinCrewRanks, String pilotLicenseNumber){
+        this.name = name;
+        this.cabinCrewRanks = cabinCrewRanks;
+        this.pilotLicenceNumber = pilotLicenseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CabinCrewRanks getCabinCrewRanks() {
+        return cabinCrewRanks;
+    }
+
+    public void setCabinCrewRanks(CabinCrewRanks cabinCrewRanks) {
+        this.cabinCrewRanks = cabinCrewRanks;
+    }
+
+    public String getPilotLicenceNumber() {
+        return pilotLicenceNumber;
+    }
+
+    public void setPilotLicenceNumber(String pilotLicenceNumber) {
+        this.pilotLicenceNumber = pilotLicenceNumber;
+    }
+}
---
+++ src/test/java/PilotTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before(){pilot = new Pilot("Sheila", CabinCrewRanks.CAPTAIN, "3233");}
+
+    @Test
+    public void hasName(){
+        assertEquals("Sheila", pilot.getName());
+    }
+
+    @Test
+    public void canSetName(){
+        pilot.setName("Henry");
+        assertEquals("Henry", pilot.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(CabinCrewRanks.CAPTAIN, pilot.getCabinCrewRanks());
+    }
+
+    @Test
+    public void canSetRank(){
+        pilot.setCabinCrewRanks(CabinCrewRanks.FIRST_OFFICER);
+        assertEquals(CabinCrewRanks.FIRST_OFFICER, pilot.getCabinCrewRanks());
+    }
+
+    @Test
+    public void hasLicense(){
+        assertEquals("3233", pilot.getPilotLicenceNumber());
+    }
+
+    @Test
+    public void canSetLicense(){
+        pilot.setPilotLicenceNumber("9655");
+        assertEquals("9655", pilot.getPilotLicenceNumber());
+    }
+
+
+}
